7537756962a95d023b8cab682a74c6b774f834d2
milapesa <milapesamazzola@gmail.com>
2024-06-28 16:07:39 +0100

    Adding splash screen to the project and new fonts.

--- app/src/main/java/com/example/final_tp3/ui/view/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/final_tp3/ui/view/activities/SplashScreenActivity.kt
@@ -1,8 +1,12 @@
 package com.example.final_tp3.ui.view.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.final_tp3.MainActivity
 import com.example.final_tp3.R
 
 class SplashScreenActivity : AppCompatActivity() {
@@ -10,5 +14,12 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+        }, 3000)
     }
 }
